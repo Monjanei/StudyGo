20c04f42c7ec93b1b320b71d75f54aefb06bcb2e
S Q <2712873020@qq.com>
2026-01-21 23:39:19 +0800

    8.switch

--- GoCode/go_study/8.switch.go
+++ GoCode/go_study/8.switch.go
@@ -4,6 +4,29 @@ import "fmt"
 
 func main() {
 	var age int
+	fmt.Printf("请输入你的年龄：")
 	fmt.Scan(&age)
-	fmt.Printf("age: %d\n", age)
+	switch {
+	case age <= 0:
+		fmt.Println("未出生")
+	case age <= 18:
+		fmt.Println("未成年")
+		fallthrough
+	case age <= 35:
+		fmt.Println("青年")
+	default:
+		fmt.Println("中年")
+	}
+
+	var week int
+	fmt.Printf("请输入星期几：")
+	fmt.Scan(&week)
+	switch week {
+	case 1, 2, 3, 4:
+		fmt.Println("😭")
+	case 5:
+		fmt.Println("😊")
+	case 6, 7:
+		fmt.Println("非常😊")
+	}
 }
